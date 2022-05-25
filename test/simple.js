c623f0577ebbe0dad3d999f008ad56a30a1221f2
Jérémy Lal <kapouer@melix.org>
2022-05-25 18:18:53 +0200

    Test paper size

--- test/simple.js
+++ test/simple.js
@@ -2,9 +2,13 @@ const express = require('express');
 const assert = require('node:assert').strict;
 const { once } = require('node:events');
 const { request } = require('undici');
+const { promisify } = require('util');
+const exec = promisify(require('node:child_process').exec);
+const tempfile = require('tempfile');
 
 const dom = require('express-dom');
 const pdf = require('..');
+const { unlink, writeFile } = require('node:fs/promises');
 
 dom.settings.verbose = true;
 
@@ -14,6 +18,25 @@ pdf.presets.x3 = {
 	icc: 'ISOcoated_v2_300_eci.icc',
 	condition: 'FOGRA39L'
 };
+
+
+async function getBox(pdfFile) {
+	const bbox = await exec(`gs -dQUIET -dNOSAFER -dBATCH -sFileName=${pdfFile} -c "FileName (r) file runpdfbegin 1 1 pdfpagecount {pdfgetpage /MediaBox get {=print ( ) print} forall (\n) print} for quit"`);
+	const [x, y, w, h] = bbox.stdout.trim().split(' ').map(x => Math.round(x * 0.35277778));
+	return { x, y, w, h };
+}
+
+async function assertBox(buf, width, height) {
+	const pdfFile = tempfile(".pdf");
+	await writeFile(pdfFile, buf);
+	try {
+		const { w, h } = await getBox(pdfFile);
+		assert.equal(w, width, "bad paper width");
+		assert.equal(h, height, "bad paper height");
+	} finally {
+		await unlink(pdfFile);
+	}
+}
 
 describe("Simple setup", function () {
 	this.timeout(10000);
@@ -42,8 +65,10 @@ describe("Simple setup", function () {
 			headers['content-disposition'],
 			'attachment; filename="test-title.pdf"'
 		);
-		const len = (await body.arrayBuffer()).length;
+		const buf = await body.arrayBuffer();
+		const len = buf.length;
 		assert.ok(len >= 100000);
+		await assertBox(buf, 216, 279);
 	});
 
 	it("compresses pdf with gs screen quality", async () => {
@@ -51,8 +76,10 @@ describe("Simple setup", function () {
 			statusCode, body
 		} = await request(`${host}/index.html?pdf=screen`);
 		assert.equal(statusCode, 200);
-		const len = (await body.arrayBuffer()).length;
+		const buf = await body.arrayBuffer();
+		const len = buf.length;
 		assert.ok(len <= 45000);
+		await assertBox(buf, 216, 279);
 	});
 
 	it("compresses high-quality pdf with prepress quality", async () => {
@@ -61,9 +88,9 @@ describe("Simple setup", function () {
 		} = await request(`${host}/index.html?pdf=prepress`);
 		assert.equal(statusCode, 200);
 		const buf = await body.arrayBuffer();
-		await require('fs/promises').writeFile('test.pdf', buf);
 		assert.ok(buf.length <= 65000);
 		assert.ok(buf.length >= 55000);
+		await assertBox(buf, 216, 279);
 	});
 
 	it("get a pdf x3 pdf with predefined icc profile", async () => {
@@ -73,6 +100,7 @@ describe("Simple setup", function () {
 		assert.equal(statusCode, 200);
 		const buf = await body.arrayBuffer();
 		assert.ok(buf.length >= 1500000);
+		await assertBox(buf, 216, 279);
 	});
 
 });
