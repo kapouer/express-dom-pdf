5d2cd7d4865285f3329fd3d7e59930b65d363850
Jérémy Lal <kapouer@melix.org>
2022-11-14 00:10:52 +0100

    Drop dead code

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const PdfHandler = require('./lib/handler');
 module.exports = new Proxy(PdfHandler, {
 	apply: (target, thisArg, args) => {
 		const h = new PdfHandler(...args);
-		h.chain.route = phase => {
-			h.route(phase);
-			return h.chain;
-		};
 		return h.chain;
 	},
 	get(...args) {
